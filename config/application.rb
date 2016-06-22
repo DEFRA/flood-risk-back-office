4163bdb7f18f32fa6abfb0c1522a614e3056c18d
Tim Crowe <tim@woodpigeon.com>
2016-06-22 17:11:52 +0100

    Fix :back link behaviour
    Ensure e.g. link_to('Back', :back) does not echo othe referrer
    which can lead to XSS. Do this by back-porting the fix from this
    in Rails 5.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require_relative "../lib/core_ext/action_view/helpers/url_helper"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
