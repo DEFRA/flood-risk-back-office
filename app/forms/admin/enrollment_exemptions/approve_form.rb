ec7ebc68a84bf0738e0adda0164df57758199e4a
Rob Nichols <robnichols.ea@gmail.com>
2016-06-29 13:55:21 +0100

    Add Approve controller

---
+++ app/forms/admin/enrollment_exemptions/approve_form.rb
@@ -0,0 +1,52 @@
+module Admin
+  module EnrollmentExemptions
+    class ApproveForm < BaseForm
+      COMMENT_MAX_LENGTH = 500
+
+      attr_reader :user
+
+      def initialize(enrollment_exemption, user)
+        @user = user
+        super(enrollment_exemption)
+      end
+      alias enrollment_exemption model
+      delegate :enrollment, :exemption, to: :enrollment_exemption
+      delegate :reference_number, to: :enrollment
+
+      def params_key
+        :admin_enrollment_exemptions_approve
+      end
+
+      def self.locale_key
+        "admin.enrollment_exemptions.approve.new"
+      end
+
+      property :comment, virtual: true
+      validates(
+        :comment,
+        length: {
+          maximum: COMMENT_MAX_LENGTH,
+          message: t(".errors.comment.too_long", max: COMMENT_MAX_LENGTH)
+        }
+      )
+
+      def comment_max_length
+        COMMENT_MAX_LENGTH
+      end
+
+      def save
+        create_comment if comment.present?
+        enrollment_exemption.approved!
+        # TODO: Trigger email
+      end
+
+      def create_comment
+        enrollment_exemption.comments.create(
+          content: comment,
+          user: user,
+          event: "Approve exemption"
+        )
+      end
+    end
+  end
+end
---
+++ spec/controllers/admin/enrollment_exemptions/approve_controller_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+module Admin
+  module EnrollmentExemptions
+    RSpec.describe ApproveController, type: :controller do
+      include Devise::TestHelpers
+
+      render_views
+      let(:enrollment_exemption) do
+        FactoryGirl.create(
+          :enrollment_exemption,
+          status: FloodRiskEngine::EnrollmentExemption.statuses[:pending]
+        )
+      end
+      let(:user) do
+        user = FactoryGirl.create(:user)
+        user.add_role :system
+        user
+      end
+
+      before do
+        sign_in user
+      end
+
+      describe "new action" do
+        it "renders page" do
+          get :new, enrollment_exemption_id: enrollment_exemption
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      describe "create action" do
+        context "with success" do
+          before do
+            allow_any_instance_of(ApproveForm).to receive(:validate).and_return(true)
+            allow_any_instance_of(ApproveForm).to receive(:save).and_return(true)
+          end
+
+          it "redirects to enrollment_exemption show" do
+            post :create, enrollment_exemption_id: enrollment_exemption
+            expect(response).to redirect_to(
+              admin_enrollment_exemption_path(enrollment_exemption)
+            )
+          end
+        end
+
+        context "with failure" do
+          before do
+            allow_any_instance_of(ApproveForm).to receive(:validate).and_return(false)
+          end
+
+          it "renders new template" do
+            post :create, enrollment_exemption_id: enrollment_exemption
+            expect(response).to have_http_status(:success)
+            expect(response).to render_template(:new)
+          end
+        end
+      end
+    end
+  end
+end
