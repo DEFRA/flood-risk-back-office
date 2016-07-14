92d142bb594dca0834cd483f02207d09fbea4f67
tom statter <github@autotelik.co.uk>
2016-07-14 16:32:53 +0000

    Grant super-user change status Ensure Edit buttons not visible for Incomplete

--- spec/features/admin/view_enrollment_exemption_spec.rb
+++ spec/features/admin/view_enrollment_exemption_spec.rb
@@ -1,132 +1,170 @@
 RSpec.feature "View Enrollment Exemption Detail" do
-  background do
-    user = create :user
-    user.add_role :system
-    login_as user
-  end
-
-  context("with secondary contact ") do
-    let(:enrollment) { create :confirmed, submitted_at: Time.zone.now }
-    let(:enrollment_exemption) do
-      enrollment.enrollment_exemptions.first.tap(&:pending!)
+  context("Enrollment NOT Submitted") do
+    before(:each) do
+      user = create :user
+      user.add_role :system
+      login_as user
     end
 
-    scenario "Page has the expected content" do
-      visit admin_enrollment_exemption_path(enrollment_exemption)
+    incomplete = FactoryGirl.factories.collect(&:name).grep(/page/).tap do |a| a.delete :page_confirmation end
 
-      within "#registration-details" do
-        expect(page).to have_css("td", text: enrollment.reference_number)
-      end
+    enrollments = incomplete.collect do |f| FactoryGirl.create(f) end
 
-      within "#correspondence-contact-details" do
-        expect(page).to have_css("strong", text: enrollment.correspondence_contact.full_name)
-      end
+    enrollments.each do |enrollment|
+      next unless enrollment.enrollment_exemptions.first
 
-      within "#secondary-contact-details" do
-        expect(page).to have_css("strong", text: enrollment.secondary_contact.full_name)
-      end
+      it "does not display any actions when not sumbitted #{enrollment.step}" do
+        enrollment_exemption = enrollment.enrollment_exemptions.first
 
-      within ".panel.actions" do
-        expect(page).to have_css("#update-enrollment-exemption-status")
-        expect(page).not_to have_link(
-          "Deregister",
-          href: new_admin_enrollment_exemption_deregister_path(
-            enrollment.enrollment_exemptions.first
-          )
-        )
-        expect(page).to have_link(
-          "Reject",
-          href: new_admin_enrollment_exemption_reject_path(
-            enrollment.enrollment_exemptions.first
-          )
-        )
-        expect(page).to have_link(
-          "Approve",
-          href: new_admin_enrollment_exemption_approve_path(
-            enrollment.enrollment_exemptions.first
-          )
-        )
-        expect(page).to have_link(
-          "Withdraw",
-          href: new_admin_enrollment_exemption_withdraw_path(
-            enrollment.enrollment_exemptions.first
-          )
-        )
+        next unless enrollment_exemption
 
-        expect(page).to have_css("#change-assisted-digital")
-      end
-    end
+        enrollment_exemption.building!
 
-    context("when the enrollment exemption is approved") do
-      before do
-        enrollment_exemption.approved!
-        enrollment.reload
-      end
+        visit admin_enrollment_exemption_path(enrollment_exemption.id)
 
-      scenario "Page has the modified content" do
-        visit admin_enrollment_exemption_path(enrollment_exemption)
+        expect(page).to have_css("#exemption-details")
 
         within ".panel.actions" do
-          expect(page).to have_link(
-            "Deregister",
-            href: new_admin_enrollment_exemption_deregister_path(enrollment_exemption)
-          )
-
-          expect(page).not_to have_link(
-            "Reject",
-            href: new_admin_enrollment_exemption_reject_path(enrollment_exemption)
-          )
-
-          expect(page).not_to have_link(
-            "Approve",
-            href: new_admin_enrollment_exemption_approve_path(
-              enrollment.enrollment_exemptions.first
-            )
-          )
-
-          expect(page).not_to have_link(
-            "Withdraw",
-            href: new_admin_enrollment_exemption_withdraw_path(
-              enrollment.enrollment_exemptions.first
-            )
-          )
+          expect(page).not_to have_link("Edit registration")
+          expect(page).not_to have_link("Deregister")
+          expect(page).not_to have_link("Reject")
+          expect(page).not_to have_link("Approve")
+          expect(page).not_to have_link("Withdraw")
         end
       end
     end
   end
 
-  context("without secondary contact ") do
-    let(:enrollment) { create :confirmed_no_secondary_contact }
-    let(:enrollment_exemption) do
-      create(
-        :enrollment_exemption,
-        enrollment: enrollment
-      )
+  context("COMPLETE") do
+    background do
+      user = create :user
+      user.add_role :system
+      login_as user
     end
 
-    scenario "Page has the expected content when optional secondary_contact not present" do
-      visit admin_enrollment_exemption_path(enrollment_exemption)
+    context("When Enrollment Submitted") do
+      let(:enrollment) { create :confirmed, submitted_at: Time.zone.now }
+      let(:enrollment_exemption) do
+        enrollment.enrollment_exemptions.first.tap(&:pending!)
+      end
 
-      within "#admin-enrollment-exemptions-show" do
-        expect(page).to have_css("#correspondence-contact-details")
-        expect(page).to_not have_css("#secondary-contact-details")
+      scenario "Page has the expected content - Status Pending" do
+        visit admin_enrollment_exemption_path(enrollment_exemption)
+
+        within "#registration-details" do
+          expect(page).to have_css("td", text: enrollment.reference_number)
+        end
+
+        within "#correspondence-contact-details" do
+          expect(page).to have_css("strong", text: enrollment.correspondence_contact.full_name)
+        end
+
+        within "#secondary-contact-details" do
+          expect(page).to have_css("strong", text: enrollment.secondary_contact.full_name)
+        end
+
+        ee = enrollment.enrollment_exemptions.first
+
+        within ".panel.actions" do
+          expect(page).to have_css("#update-enrollment-exemption-status")
+          expect(page).not_to have_link(
+            "Deregister", href: new_admin_enrollment_exemption_deregister_path(ee)
+          )
+
+          expect(page).to have_link(
+            "Reject",
+            href: new_admin_enrollment_exemption_reject_path(
+              enrollment.enrollment_exemptions.first
+            )
+          )
+          expect(page).to have_link(
+            "Approve",
+            href: new_admin_enrollment_exemption_approve_path(
+              enrollment.enrollment_exemptions.first
+            )
+          )
+          expect(page).to have_link(
+            "Withdraw",
+            href: new_admin_enrollment_exemption_withdraw_path(
+              enrollment.enrollment_exemptions.first
+            )
+          )
+
+          expect(page).to have_css("#change-assisted-digital")
+        end
+      end
+
+      context "Page has the expected content - Status APPROVED" do
+        before do
+          enrollment_exemption.approved!
+          enrollment.reload
+        end
+
+        scenario "Page has the modified content" do
+          visit admin_enrollment_exemption_path(enrollment_exemption)
+
+          within ".panel.actions" do
+            expect(page).to have_link(
+              "Deregister",
+              href: new_admin_enrollment_exemption_deregister_path(enrollment_exemption)
+            )
+
+            expect(page).not_to have_link(
+              "Reject",
+              href: new_admin_enrollment_exemption_reject_path(enrollment_exemption)
+            )
+
+            expect(page).not_to have_link(
+              "Approve",
+              href: new_admin_enrollment_exemption_approve_path(
+                enrollment.enrollment_exemptions.first
+              )
+            )
+
+            expect(page).not_to have_link(
+              "Withdraw",
+              href: new_admin_enrollment_exemption_withdraw_path(
+                enrollment.enrollment_exemptions.first
+              )
+            )
+          end
+        end
       end
     end
-  end
 
-  context("when organisation is partnership") do
-    let(:enrollment) { create :submitted_partnership }
+    context("without secondary contact ") do
+      let(:enrollment) { create :confirmed_no_secondary_contact }
+      let(:enrollment_exemption) do
+        create(
+          :enrollment_exemption,
+          enrollment: enrollment
+        )
+      end
 
-    scenario "Page has expected content for all partners", duff: true do
-      visit admin_enrollment_exemption_path(enrollment.enrollment_exemptions.first.id)
+      scenario "Page has the expected content when optional secondary_contact not present" do
+        visit admin_enrollment_exemption_path(enrollment_exemption)
 
-      within "#admin-enrollment-exemptions-show" do
-        expect(enrollment.organisation.partners.size).to be > 0
+        within "#admin-enrollment-exemptions-show" do
+          expect(page).to have_css("#correspondence-contact-details")
+          expect(page).to_not have_css("#secondary-contact-details")
+        end
+      end
+    end
 
-        # factory generates a random number of partners  -  they should all be represented
-        enrollment.organisation.partners.each_with_index do |p, i|
-          expect(page).to have_text p.full_name
-          expect(page).to have_text "Partner #{i + 1}"
+    context("when organisation is partnership") do
+      let(:enrollment) { create :submitted_partnership }
+
+      scenario "Page has expected content for all partners" do
+        visit admin_enrollment_exemption_path(enrollment.enrollment_exemptions.first.id)
+
+        within "#admin-enrollment-exemptions-show" do
+          expect(enrollment.organisation.partners.size).to be > 0
+
+          # factory generates a random number of partners  -  they should all be represented
+          enrollment.organisation.partners.each_with_index do |p, i|
+            expect(page).to have_text p.full_name
+            expect(page).to have_text "Partner #{i + 1}"
+          end
         end
       end
     end
