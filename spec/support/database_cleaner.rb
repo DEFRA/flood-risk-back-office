9b76d0afc330e3bb5774c04c4271bd04e02cf703
irisfaraway <iris.faraway@gmail.com>
2019-06-18 15:24:40 +0100

    Change cleaning strategy depending on test type
    This logic was previously in the config. Feature tests raise database errors without it.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -12,6 +12,12 @@ RSpec.configure do |config|
   end
 
   config.around(:each) do |example|
+    if example.metadata[:type] == :feature
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+
     DatabaseCleaner.cleaning do
       example.run
     end
