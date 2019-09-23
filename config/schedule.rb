11d2cd7cd1bda449133fd4d6eda0f15e311cfa78
cintamani <cintamani.puddu@gmail.com>
2019-09-23 15:39:18 +0100

    Add correct task to scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,5 +29,5 @@ set :job_template, "/bin/bash -l -c 'eval \"$(rbenv init -)\" && :job'"
 
 # This will run daily and update EA areas for addresses with x and y but without Area.
 every :day, at: (ENV["EA_AREA_LOOKUP"] || "1:05"), roles: [:db] do
-  rake ""
+  rake "ea_lookups:update:area"
 end
