714c4139bd54a34e682fd1d6952d336b3ba67839
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-10 13:55:17 +0100

    Update gem and add new config
    The gem is written such that if the env var is not set it will read the config as being false.

--- config/initializers/defra_ruby_aws.rb
+++ config/initializers/defra_ruby_aws.rb
@@ -7,7 +7,8 @@ DefraRuby::Aws.configure do |c|
     credentials: {
       access_key_id: ENV["AWS_DAILY_EXPORT_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_DAILY_EXPORT_SECRET_ACCESS_KEY"]
-    }
+    },
+    encrypt_with_kms: ENV["AWS_ENCRYPT_WITH_KMS"]
   }
 
   c.buckets = [epr_bucket]
