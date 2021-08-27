892daf1d5cce3f24d8eb4902907adb390deab191
Toby Privett <toby@snaplab.co.uk>
2021-08-27 14:46:48 +0100

    Enable some flash messages
    - Here we set a green flash for 'notice' and a blue flash for
    everything else
    
    - Some of the controllers (eg admin_enrollments#update) don't
    set flash messages so we should check these when we
    style the forms

--- spec/support/matchers/have_flash.rb
+++ spec/support/matchers/have_flash.rb
@@ -1,69 +1,11 @@
-RSpec::Matchers.define :have_flash do |text, key: :notice|
-  match do |_actual|
-    expect(page).to have_css(alert_css(key), text: text)
-  end
-
-  failure_message do |_actual|
-    %(expected to see a HTML element [#{alert_css key}], with text "#{text}")
-  end
-
-  failure_message_when_negated do |_actual|
-    %(expected not to see a HTML element [#{alert_css key}], with text "#{text}")
-  end
-
-  description do
-    %(display a HTML element [#{alert_css key}], with text "#{text}")
-  end
-
-  private
-
-  def alert_css(key)
-    # Map flash keys to Bootstrap CSS classes
-    clazz =
-      case key
-      when :alert then "danger"
-      when :notice then "success"
-      when :info then "info"
-      when :warn then "warning"
-      when :not_authorized then "danger"
-      else "alert"
-      end
-
-    ".alert.alert-#{clazz}"
+RSpec::Matchers.define :have_flash do |text|
+  match do
+    expect(page).to have_css(".govuk-notification-banner", text: text)
   end
 end
 
-RSpec::Matchers.define :have_no_flash do |key = :notice|
-  match do |_actual|
-    expect(page).to have_no_css alert_css(key)
-  end
-
-  failure_message do |_actual|
-    %(expected not to see a HTML element [#{alert_css key}])
-  end
-
-  failure_message_when_negated do |_actual|
-    %(expected to see a HTML element [#{alert_css key}])
-  end
-
-  description do
-    %(not display a HTML element [#{alert_css key}])
-  end
-
-  private
-
-  def alert_css(key)
-    # Map flash keys to Bootstrap CSS classes
-    clazz =
-      case key
-      when :alert then "danger"
-      when :notice then "success"
-      when :info then "info"
-      when :warn then "warning"
-      when :not_authorized then "danger"
-      else "alert"
-      end
-
-    ".alert.alert-#{clazz}"
+RSpec::Matchers.define :have_no_flash do
+  match do
+    expect(page).to have_no_css alert_css(".govuk-notification-banner")
   end
 end
