fcdfffcc1254c19dc7114ecc3de0d5c90d18fb18
Toby Privett <toby@snaplab.co.uk>
2021-09-01 10:06:39 +0100

    Refactor search
    https://eaflood.atlassian.net/browse/RUBY-1477

--- app/queries/enrollment_exemption_search_query.rb
+++ app/queries/enrollment_exemption_search_query.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# A query object to rapw up the nasty gubbins behind pseudo full-text searching
-# for enrollment exemptions. In an ideal world you might do this using elastic search.
-# Here we use squeel to make to querying less painful, and the pattern of mixing
-# custom scope into the relation to make the code easy(er) to read.
 class EnrollmentExemptionSearchQuery
   attr_reader :relation
 
@@ -12,73 +8,72 @@ class EnrollmentExemptionSearchQuery
   end
 
   def initialize
-    # Note the .joins (using squeel for brevity) help us build the search query, and the
-    # .includes make sure we don't have any N+1 queries
-    @relation = FloodRiskEngine::EnrollmentExemption.limit(100)
-    # .joins { enrollment.organisation.outer.primary_address.outer }
-    # .joins { enrollment.correspondence_contact.outer }
-    # .joins { enrollment.exemption_location.outer }
-    # .joins { enrollment.reference_number.outer }
-    # .joins { exemption }
-    # .includes(:exemption,
-    #           enrollment: [
-    #             :reference_number,
-    #             {
-    #               organisation: [:primary_address]
-    #             }
-    #           ])
-    # .extending(Scopes)
+    @relation =
+      FloodRiskEngine::EnrollmentExemption
+      .joins(
+        :exemption,
+        enrollment:
+          [
+            :reference_number,
+            :correspondence_contact,
+            :exemption_location,
+            { organisation: :primary_address }
+          ]
+      )
+      .extending(Scopes)
   end
 
-  # Note .page and .per_page are kaminari methods
   def call(search_form)
     relation
-      # .having_status(search_form.status)
-      # .matching_query(search_form.q)
-      # .sort
+      .having_status(search_form.status)
+      .matching_query(search_form.q)
+      .sort
       .page(search_form.page)
       .per(search_form.per_page)
   end
 
   module Scopes
 
-    # rubocop:disable Metrics/AbcSize
-    # rubocop:disable Naming/MethodParameterName
-    def matching_query(q)
-      return all if q.blank?
+    def matching_query(query_to_match)
+      return all if query_to_match.blank?
 
-      query = SearchTerm.new(q)
-      where do
-        (exemption.code =~ query.q) |
-          (replace(enrollment.organisation.primary_address.postcode, " ", "") =~ query.fuzzy_without_whitespace) |
-          (replace(enrollment.exemption_location.grid_reference, " ", "") =~ query.without_whitespace) |
-          (enrollment.organisation.name =~ query.fuzzy) |
-          (enrollment.organisation.searchable_content =~ query.fuzzy) |
-          (enrollment.reference_number.number =~ query.fuzzy) |
-          (enrollment.correspondence_contact.full_name =~ query.fuzzy)
-      end
+      query = SearchTerm.new(query_to_match)
+
+      where(flood_risk_engine_reference_numbers: { number: query.q })
+        .or(where("flood_risk_engine_exemptions.code LIKE ?", query.fuzzy))
+        .or(where("UPPER(flood_risk_engine_organisations.name) LIKE ?", query.fuzzy))
+        .or(where("UPPER(flood_risk_engine_contacts.full_name) LIKE ?", query.fuzzy))
+        .or(
+          where("UPPER(REPLACE(flood_risk_engine_addresses.postcode, ' ','')) LIKE ?",
+                query.fuzzy_without_whitespace)
+        )
+        .or(
+          where("UPPER(REPLACE(flood_risk_engine_locations.grid_reference, ' ','')) LIKE ?",
+                query.fuzzy_without_whitespace)
+        )
     end
-    # rubocop:enable Metrics/AbcSize
-    # rubocop:enable Naming/MethodParameterName
 
     def having_status(target_status)
       return all if target_status.blank?
 
-      where { status == FloodRiskEngine::EnrollmentExemption.statuses[target_status] }
+      where(status: FloodRiskEngine::EnrollmentExemption.statuses[target_status])
     end
 
     def sort
-      order { [enrollment.submitted_at.asc, enrollment.created_at.asc] }
+      order(
+        [
+          "flood_risk_engine_enrollments.submitted_at ASC",
+          "flood_risk_engine_enrollments.created_at ASC"
+        ]
+      )
     end
 
     class SearchTerm
       attr_reader :q
 
-      # rubocop:disable Naming/MethodParameterName
-      def initialize(q = "")
-        @q = q.strip
+      def initialize(query_to_search = "")
+        @q = query_to_search.strip.upcase
       end
-      # rubocop:enable Naming/MethodParameterName
 
       def without_whitespace
         @without_whitespace ||= @q.delete(" ")
--- spec/queries/enrollment_exemption_search_query_spec.rb
+++ spec/queries/enrollment_exemption_search_query_spec.rb
@@ -1,5 +1,78 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
-RSpec.describe EnrollmentExemptionSearchQuery, type: :form do
-  let(:search_form) { SearchForm.new({}) }
+RSpec.describe EnrollmentExemptionSearchQuery, type: :query do
+  let!(:approved_individual) { create(:approved_individual) }
+  let!(:rejected_individual) { create(:rejected_individual) }
+
+  let(:search_term) { "" }
+  let(:status) { nil }
+
+  let(:search_form) do
+    SearchForm.new(
+      {
+        search: {
+          q: search_term,
+          status: status
+        }
+      }
+    )
+  end
+
+  subject { described_class.call(search_form).all.map(&:enrollment) }
+
+  describe "filter by status" do
+    it "finds all the enrollments" do
+      expect(subject).to eq([approved_individual, rejected_individual])
+    end
+
+    context "status = approved" do
+      let(:status) { :approved }
+
+      it "finds the approved enrollment" do
+        expect(subject).to eq([approved_individual])
+      end
+    end
+  end
+
+  describe "by something that doesn't exist" do
+    let(:search_term) { "foo_bar_xyz" }
+
+    it "finds nothing" do
+      expect(subject).to be_empty
+    end
+  end
+
+  describe "by reference_number" do
+    let(:search_term) { approved_individual.reference_number.downcase }
+
+    it "finds the approved enrollment" do
+      expect(subject).to eq([approved_individual])
+    end
+  end
+
+  describe "by organisation name" do
+    let(:search_term) { approved_individual.organisation.name }
+
+    it "finds the approved enrollment" do
+      expect(subject).to eq([approved_individual])
+    end
+  end
+
+  describe "by contact name" do
+    let(:search_term) { approved_individual.correspondence_contact.full_name }
+
+    it "finds the approved enrollment" do
+      expect(subject).to eq([approved_individual])
+    end
+  end
+
+  describe "by postcode" do
+    let(:search_term) { "Bs15Ah" }
+
+    it "finds all the enrollments" do
+      expect(subject).to eq([approved_individual, rejected_individual])
+    end
+  end
 end
