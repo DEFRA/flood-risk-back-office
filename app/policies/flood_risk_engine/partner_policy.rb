f017c26fe4afccc97949c9fb770310caff625b88
Toby Privett <toby@snaplab.co.uk>
2021-09-13 14:08:06 +0100

    Styling: Edit address (#323)
    https://eaflood.atlassian.net/browse/RUBY-1495
    * Introduce an Address model
    * Refactor the Address controller
    * Fix Search so that Partnerships are displayed
    * Introduce a PartnerAddress model
    * Update a Partner's Name and Address
    * Tidy up the unused Partner Forms code
    * Prefer bold labels for Name and Address
    * Move specs out of the admin scope

--- app/policies/flood_risk_engine/partner_policy.rb
+++
@@ -1,8 +0,0 @@
-module FloodRiskEngine
-  class PartnerPolicy < ApplicationPolicy
-    def edit?
-      system_user? || super_agent_user? || admin_agent_user?
-    end
-    alias update? edit?
-  end
-end
