d02d864a9a8fe5432709604bfa34777dd5666cb1
Toby Privett <toby@snaplab.co.uk>
2021-09-14 10:06:02 +0100

    Validate the postcode format
    https://eaflood.atlassian.net/browse/RUBY-1495?focusedCommentId=332108

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,9 +4,13 @@ class Address < ActiveRecord::Base
   validates :premises, presence: true
   validates :street_address, presence: true
   validates :city, presence: true
-  validates :postcode, presence: true
+  validates :postcode, "flood_risk_engine/postcode": { presence: true }
 
   def to_param
     token
+  end
+
+  def address_finder_errored!
+    # Invoked by postcode validator, but we don't want/need it here
   end
 end
