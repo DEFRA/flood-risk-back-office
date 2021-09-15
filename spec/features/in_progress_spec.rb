ea6732d30dbd7285cd95003e32983da74a8c23d5
Toby Privett <toby@snaplab.co.uk>
2021-09-15 16:02:54 +0100

    Styling: Mark registration as in progress
    https://eaflood.atlassian.net/browse/RUBY-1601

---
+++ spec/features/in_progress_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "In progress", type: :feature do
+  let(:enrollment_exemption) { create(:submitted_limited_company).enrollment_exemptions.first }
+  let(:user) { create(:user) }
+
+  before do
+    enrollment_exemption.update(status: :pending)
+    user.grant :system
+    login_as user
+    visit admin_enrollment_exemption_path(enrollment_exemption)
+
+    within("#actions") { click_link("In Progress") }
+  end
+
+  scenario "successfully" do
+    click_on "Confirm In Progress status"
+
+    within("#status") { expect(page).to have_text("In progress") }
+    within("#comment-history") { expect(page).to have_text("Status changed to In Progress") }
+  end
+end
