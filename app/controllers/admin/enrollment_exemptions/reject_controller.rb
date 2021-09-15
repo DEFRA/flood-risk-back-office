61d16cc7c9ef68a641a640807e0f6c3910080331
Toby Privett <toby@snaplab.co.uk>
2021-09-15 14:48:56 +0100

    Styling: Reject registration
    https://eaflood.atlassian.net/browse/RUBY-1500

--- app/controllers/admin/enrollment_exemptions/reject_controller.rb
+++ app/controllers/admin/enrollment_exemptions/reject_controller.rb
@@ -1,17 +1,49 @@
 module Admin
   module EnrollmentExemptions
-    class RejectController < ChangeStatusBaseController
+    class RejectController < ApplicationController
+      before_action :load_and_authorise_enrollment_exemption
+
+      def new
+        load_presenter
+      end
+
+      def create
+        if @enrollment_exemption.action!(enrollment_exemption_params)
+          SendRegistrationRejectedEmail.for(@enrollment_exemption)
+          redirect_to admin_enrollment_exemption_path(@enrollment_exemption)
+        else
+          load_presenter
+          render :new
+        end
+      end
 
       private
 
-      def authorize_action
-        authorize enrollment_exemption, :reject?
+      def enrollment_exemption_params
+        params.require(:enrollment_exemption).permit(
+          :comment_content
+        ).merge(rejection_params)
       end
 
-      def form_class
-        RejectForm
+      def rejection_params
+        {
+          status: :rejected,
+          comment_event: "Rejected exemption",
+          accept_reject_decision_user_id: current_user.id,
+          accept_reject_decision_at: Time.zone.now
+        }
       end
 
+      def load_and_authorise_enrollment_exemption
+        @enrollment_exemption = EnrollmentExemption.find(params[:enrollment_exemption_id])
+
+        authorize @enrollment_exemption, :reject?
+      end
+
+      def load_presenter
+        @enrollment_exemption_presenter =
+          EnrollmentExemptionPresenter.new(@enrollment_exemption, view_context)
+      end
     end
   end
 end
