87d813ced7206e2ec4f908fe3d30cb8a19e54477
Toby Privett <toby@snaplab.co.uk>
2021-09-16 13:56:02 +0100

    Styling: Reissue registration approval email
    https://eaflood.atlassian.net/browse/RUBY-1603

---
+++ spec/features/resend_approval_email_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Resend an approval email", type: :feature do
+  let(:enrollment_exemption) { create(:approved_limited_company).enrollment_exemptions.first }
+  let(:user) { create(:user) }
+
+  before do
+    user.grant :system
+    login_as user
+    visit admin_enrollment_exemption_path(enrollment_exemption)
+
+    within("#actions") { click_link("Reissue approval email") }
+  end
+
+  scenario "successfully" do
+    expect(SendRegistrationApprovedEmail).to receive(:for).once
+
+    fill_in "Comment", with: "Reissued by Alice!"
+    click_on "Confirm and send email"
+
+    within("#comment-history") do
+      expect(page).to have_text("Reissued by Alice!")
+      expect(page).to have_text("Reissued registration approved email")
+    end
+  end
+
+  scenario "unsuccessfully" do
+    click_on "Confirm and send email"
+
+    expect(page).to have_css(".govuk-error-message", text: "Enter a comment")
+  end
+end
