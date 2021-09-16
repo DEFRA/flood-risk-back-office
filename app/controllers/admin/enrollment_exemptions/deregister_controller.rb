add4fef4019586eff75dc939f9907bdf20b033ed
Toby Privett <toby@snaplab.co.uk>
2021-09-16 11:27:13 +0100

    Styling: Deregister registration
    https://eaflood.atlassian.net/browse/RUBY-1602

--- app/controllers/admin/enrollment_exemptions/deregister_controller.rb
+++ app/controllers/admin/enrollment_exemptions/deregister_controller.rb
@@ -1,17 +1,54 @@
 module Admin
   module EnrollmentExemptions
-    class DeregisterController < ChangeStatusBaseController
+    class DeregisterController < ApplicationController
+      before_action :load_and_authorise_enrollment_exemption
+
+      def new
+        load_page_defaults
+      end
+
+      def create
+        if @enrollment_exemption.action!(deregister_params)
+          redirect_to admin_enrollment_exemption_path(@enrollment_exemption)
+        else
+          load_page_defaults
+          render :new
+        end
+      end
 
       private
 
-      def authorize_action
-        authorize enrollment_exemption, :deregister?
+      def enrollment_exemption_params
+        params.require(:enrollment_exemption).permit(
+          :comment_content, :deregister_reason
+        )
       end
 
-      def form_class
-        DeregisterForm
+      def deregister_params
+        enrollment_exemption_params.merge(
+          {
+            status: :deregistered,
+            comment_event: comment_event
+          }
+        )
       end
 
+      def comment_event
+        "Deregistered exemption with #{enrollment_exemption_params[:deregister_reason].to_s.humanize}"
+      end
+
+      def load_and_authorise_enrollment_exemption
+        @enrollment_exemption = EnrollmentExemption.find(params[:enrollment_exemption_id])
+
+        authorize @enrollment_exemption, :deregister?
+      end
+
+      def load_page_defaults
+        @deregister_reasons = FloodRiskEngine::EnrollmentExemption.deregister_reasons.keys
+
+        @enrollment_exemption_presenter =
+          EnrollmentExemptionPresenter.new(@enrollment_exemption, view_context)
+      end
     end
   end
 end
---
+++ spec/features/deregister_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "Deregister an enrollment", type: :feature do
+  let(:enrollment_exemption) { create(:approved_limited_company).enrollment_exemptions.first }
+  let(:user) { create(:user) }
+
+  before do
+    user.grant :system
+    login_as user
+    visit admin_enrollment_exemption_path(enrollment_exemption)
+
+    within("#actions") { click_link("Deregister") }
+  end
+
+  scenario "successfully" do
+    select "Operator failings", from: "Deregister reason"
+    fill_in "Comment", with: "Deregistered by Alice!"
+    click_on "Deregister registration"
+
+    within("#status") { expect(page).to have_text("Deregistered") }
+
+    within("#comment-history") do
+      expect(page).to have_text("Deregistered exemption with Operator failings")
+      expect(page).to have_text("Deregistered by Alice!")
+    end
+  end
+
+  scenario "unsuccessfully" do
+    click_on "Deregister registration"
+
+    expect(page).to have_css(".govuk-error-message", text: "Enter a comment")
+  end
+end
