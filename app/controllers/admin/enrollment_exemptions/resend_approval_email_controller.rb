2212c863100916eea9717b93b99e56e8ceac9f03
Toby Privett <toby@snaplab.co.uk>
2021-09-16 13:56:02 +0100

    Styling: Reissue registration approval email
    https://eaflood.atlassian.net/browse/RUBY-1603

--- app/controllers/admin/enrollment_exemptions/resend_approval_email_controller.rb
+++ app/controllers/admin/enrollment_exemptions/resend_approval_email_controller.rb
@@ -1,17 +1,40 @@
 module Admin
   module EnrollmentExemptions
-    class ResendApprovalEmailController < ChangeStatusBaseController
+    class ResendApprovalEmailController < ApplicationController
+      before_action :load_and_authorise_enrollment_exemption
+
+      def new
+        load_presenter
+      end
+
+      def create
+        if @enrollment_exemption.action!(enrollment_exemption_params)
+          SendRegistrationApprovedEmail.for(@enrollment_exemption)
+          redirect_to admin_enrollment_exemption_path(@enrollment_exemption)
+        else
+          load_presenter
+          render :new
+        end
+      end
 
       private
 
-      def authorize_action
-        authorize enrollment_exemption, :resend_approval_email?
+      def enrollment_exemption_params
+        params.require(:enrollment_exemption).permit(
+          :comment_content
+        ).merge(comment_event: "Reissued registration approved email")
       end
 
-      def form_class
-        ResendApprovalEmailForm
+      def load_and_authorise_enrollment_exemption
+        @enrollment_exemption = EnrollmentExemption.find(params[:enrollment_exemption_id])
+
+        authorize @enrollment_exemption, :resend_approval_email?
       end
 
+      def load_presenter
+        @enrollment_exemption_presenter =
+          EnrollmentExemptionPresenter.new(@enrollment_exemption, view_context)
+      end
     end
   end
 end
