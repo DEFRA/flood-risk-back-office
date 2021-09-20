1bf5c66d624e9fef1d94302616d7dae68ebb0327
Toby Privett <toby@snaplab.co.uk>
2021-09-20 15:41:00 +0100

    Cleanup old code (#337)
    * Replace duplicated method
    * Remove Policy: FloodRiskEngine::EnrollmentExemption
    * Remove Reform gem and initializer
    * Remove  gem "rails-controller-testing" and duplicate test
    * Redundant RSpec matchers
    * Remove pending specs
    * Remove no-longer-needed UI helpers

--- spec/features/admin/edit_user_role_feature_spec.rb
+++ spec/features/admin/edit_user_role_feature_spec.rb
@@ -15,37 +15,6 @@ RSpec.feature "As an System user, I want to edit another user's role" do
     User.destroy_all
   end
 
-  context "authorised" do
-    scenario "System user can edit another user's role", versioning: true do
-      pending "FIX ME!"
-      login_as system_user
-      visit edit_admin_user_path(other_user)
-
-      select "Data user", from: "Role"
-
-      expect do
-        expect do
-          click_button "Update user"
-          expect(page).to have_flash I18n.t("user_update_success", name: other_user.email)
-        end.to change { other_user.roles(true).pluck(:name) }.from(["admin_agent"]).to(["data_agent"])
-      end
-      expect(current_path).to eq admin_users_path
-
-      expect(other_user.role_names).to eq "data_agent"
-
-      # NOTE: 2 "paper-trail" versions records are created,
-      #       as any existing user role(s) are deleted before the new role is added
-      #       which results in two updates to the user record
-      # last_version = other_user.versions.last
-      # expect(last_version.reify.role_names).to be_nil
-      # expect(last_version.whodunnit).to eq system_user.id.to_s
-      #
-      # prev_version = other_user.versions[-2]
-      # expect(prev_version.reify.role_names).to eq "admin_agent"
-      # expect(prev_version.whodunnit).to eq system_user.id.to_s
-    end
-  end
-
   context "unauthorised" do
     scenario "Logged out user is asked to sign-in" do
       # don't sign in
