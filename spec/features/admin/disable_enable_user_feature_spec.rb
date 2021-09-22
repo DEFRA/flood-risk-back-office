93f9014ac9d3a0bd47b7a7e7c0f79cd23dbc0385
Toby Privett <toby@snaplab.co.uk>
2021-09-22 14:41:13 +0100

    Remove long-term pending spec

--- spec/features/admin/disable_enable_user_feature_spec.rb
+++ spec/features/admin/disable_enable_user_feature_spec.rb
@@ -13,7 +13,6 @@ RSpec.feature "As an System user, I want to disable/enable a user" do
 
   context "authorised" do
     scenario "System user can disable another user", versioning: true do
-      # pending "FIX ME!"
       system_user = create(:user).tap { |u| u.add_role :system }
       other_user = create(:user).tap { |u| u.add_role :admin_agent }
 
@@ -53,43 +52,6 @@ RSpec.feature "As an System user, I want to disable/enable a user" do
       # Devise "paranoid mode" is on, so display the message "Invalid email or password",
       # instead of the default "Your account is not activated yet" message
       expect(page).to have_flash(I18n.t("devise.failure.invalid", authentication_keys: "email"), key: :alert)
-    end
-
-    scenario "System user can enable another user", versioning: true do
-      pending "FIX ME!"
-      other_user.disable! "Testing"
-
-      login_as system_user
-      visit admin_users_path
-
-      expect do
-        expect do
-          within("tr#user_#{other_user.id}") { click_link "Enable" }
-          expect(page).to have_flash I18n.t("user_enabled_success", name: other_user.email)
-        end.to change { other_user.reload.disabled_at }.to(nil)
-      end.to change { other_user.reload.versions.count }.by(1)
-
-      expect(current_path).to eq admin_users_path
-
-      expect(other_user).to be_enabled
-      expect(other_user.disabled_at).to be_nil
-      expect(other_user.disabled_comment).to be_nil
-
-      last_version = other_user.versions.last
-      expect(last_version.reify).to be_disabled
-      expect(last_version.whodunnit).to eq system_user.id.to_s
-
-      logout :user
-      visit new_user_session_path
-      fill_in "Email", with: other_user.email
-      fill_in "Password", with: other_user.password
-
-      expect do
-        click_button "Sign in"
-      end.to change { other_user.reload.sign_in_count }.by(1)
-
-      expect(page).to have_flash I18n.t("devise.sessions.signed_in")
-      expect(current_path).to eq "/"
     end
   end
 
