45f9fb8a447a30ba35e842a9f9768f566111f118
Iris Faraway <iris.faraway@gmail.com>
2021-11-04 10:40:25 +0000

    Remove old transient_registration records (#365)
    * Remove old transient_registration records
    
    https://eaflood.atlassian.net/browse/RUBY-1685
    
    We want to clean up old transient_registration records that have been sitting around for more than 30 days.
    
    * Add to schedule and test scheduling
    
    * Fix linter issues
    
    * Change engine branch back to rails6

---
+++ app/services/transient_registration_cleanup_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class TransientRegistrationCleanupService < ::FloodRiskEngine::BaseService
+  def run
+    transient_registrations_to_remove.destroy_all
+  end
+
+  private
+
+  def transient_registrations_to_remove
+    FloodRiskEngine::TransientRegistration.where("created_at < ?", oldest_possible_date)
+  end
+
+  def oldest_possible_date
+    max = Rails.configuration.max_transient_registration_age_days.to_i
+    max.days.ago.beginning_of_day
+  end
+end
