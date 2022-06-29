8f5314cb0ece291cee7c520974e65144cf409faa
Dave G <david.georgiou@gmail.com>
2022-06-29 16:12:33 +0100

    fix export bug (#478)

--- spec/jobs/enrollment_export_job_spec.rb
+++ spec/jobs/enrollment_export_job_spec.rb
@@ -2,10 +2,14 @@ require "rails_helper"
 require "sucker_punch/testing/inline"
 
 RSpec.describe EnrollmentExportJob do
-  subject(:job) { described_class.perform_now(ee) }
+  subject(:job) { described_class.perform_now(export) }
 
   describe ".perform_later" do
-    let(:ee) { create(:enrollment_export, :with_dates, :with_file_name) }
+    # let(:enrollment) { create(:enrollment, submitted_at: Date.today) }
+    let(:enrollment) { create(:approved_individual) }
+    # let!(:exemption) { create(:enrollment_exemption, enrollment: enrollment, accept_reject_decision_at: Date.today) }
+    let!(:exemption) { enrollment.enrollment_exemptions.first }
+    let(:export) { create(:enrollment_export, :with_dates, :with_file_name) }
 
     it "does not error" do
       job
