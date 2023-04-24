4d65b1cd46061a63dcb10d750d674d6148ef2c4b
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-04-24 18:44:56 +0100

    Fix write_to_aws_s3 spec stub

--- spec/services/write_to_aws_s3_spec.rb
+++ spec/services/write_to_aws_s3_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe WriteToAwsS3, type: :service do
 
     before do
       Aws.config.update({ region: "eu-west-1", credentials: Aws::Credentials.new("key_id", "access_key") })
-      stub_request(:put, %r{https://.*\.s3\.eu-west-1\.amazonaws\.com.*})
+      stub_request(:post, %r{https://.*\.s3\.eu-west-1\.amazonaws\.com.*})
     end
 
     it "returns a presigned url structure" do
