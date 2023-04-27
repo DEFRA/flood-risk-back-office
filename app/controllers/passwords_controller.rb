8eb24503af0e4f756f24ada74be174c9f186e69e
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-04-27 10:20:33 +0100

    Remove unused devise methods

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,14 +2,8 @@ class PasswordsController < Devise::PasswordsController
   protected
 
   def after_sending_reset_password_instructions_path_for(_resource_name)
+    Rails.logger.warn ">>>>> after_sending_reset_password_instructions_path_for"
     new_user_session_path if is_navigational_format?
   end
 
-  def after_resetting_password_path_for(resource)
-    if Devise.sign_in_after_reset_password
-      after_sign_in_path_for(resource)
-    else
-      new_user_session_path
-    end
-  end
 end
