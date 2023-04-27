079dceece7d54803ddc61df9f247fb4ff30afa5b
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-04-27 10:36:02 +0100

    Remove debug line

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,7 +2,6 @@ class PasswordsController < Devise::PasswordsController
   protected
 
   def after_sending_reset_password_instructions_path_for(_resource_name)
-    Rails.logger.warn ">>>>> after_sending_reset_password_instructions_path_for"
     new_user_session_path if is_navigational_format?
   end
 
