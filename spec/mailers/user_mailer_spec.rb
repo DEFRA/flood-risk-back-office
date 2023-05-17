33667011be6319a3163d9089a5fb91d04f84501a
Leonid Brujev <brujev@gmail.com>
2023-05-17 15:50:09 +0100

    refactoring tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe UserMailer do
   end
 
   describe "invitation_instructions" do
-    let(:mail) { UserMailer.invitation_instructions(user, token) }
+    let(:mail) { described_class.invitation_instructions(user, token) }
 
     it "sets the correct template ID and personalisation" do
       expect(mail[:template_id].to_s).to eq("d13b900d-5145-481c-947b-6776afad8ea0")
@@ -39,7 +39,7 @@ RSpec.describe UserMailer do
   end
 
   describe "reset_password_instructions" do
-    let(:mail) { UserMailer.reset_password_instructions(user, token) }
+    let(:mail) { described_class.reset_password_instructions(user, token) }
 
     it "sets the correct template ID and personalisation" do
       expect(mail[:template_id].to_s).to eq("3fc1ef2d-0fa4-4f87-a319-a3b6dee34cb8")
@@ -64,7 +64,7 @@ RSpec.describe UserMailer do
   end
 
   describe "unlock_instructions" do
-    let(:mail) { UserMailer.unlock_instructions(user, token) }
+    let(:mail) { described_class.unlock_instructions(user, token) }
 
     it "sets the correct template ID and personalisation" do
       expect(mail.to).to eq([user.email])
