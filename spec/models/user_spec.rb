a47555d7240d5b6ded0cc2203246ce2c6a862de3
Leonid Brujev <brujev@gmail.com>
2023-05-18 13:00:30 +0100

    improving test coverage

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,82 @@
+require "rails_helper"
+
+RSpec.describe User do
+  let(:user) { create(:user) }
+
+  describe "#disable" do
+    it "updates the disabled_at and disabled_comment attributes" do
+      comment = "test comment"
+      user.disable(comment)
+      expect(user.disabled_at).not_to be_nil
+      expect(user.disabled_comment).to eq(comment)
+    end
+  end
+
+  describe "#disable!" do
+    it "updates the disabled_at and disabled_comment attributes and saves the record" do
+      comment = "test comment"
+      user.disable!(comment)
+      expect(user.disabled_at).not_to be_nil
+      expect(user.disabled_comment).to eq(comment)
+      expect(user.reload.disabled_at).not_to be_nil
+      expect(user.reload.disabled_comment).to eq(comment)
+    end
+  end
+
+  describe "#enable!" do
+    it "clears the disabled_at and disabled_comment attributes and saves the record" do
+      user.disable!("test comment")
+      user.enable!
+      expect(user.disabled_at).to be_nil
+      expect(user.disabled_comment).to be_nil
+      expect(user.reload.disabled_at).to be_nil
+      expect(user.reload.disabled_comment).to be_nil
+    end
+  end
+
+  describe "#disabled?" do
+    it "returns true if the user is disabled" do
+      user.disable!("test comment")
+      expect(user).to be_disabled
+    end
+
+    it "returns false if the user is not disabled" do
+      expect(user).not_to be_disabled
+    end
+  end
+
+  describe "#enabled?" do
+    it "returns true if the user is enabled" do
+      expect(user).to be_enabled
+    end
+
+    it "returns false if the user is disabled" do
+      user.disable!("test comment")
+      expect(user).not_to be_enabled
+    end
+  end
+
+  describe "#password_meets_minimum_requirements" do
+    it "validates that the password meets the minimum length requirements" do
+      user.password = "pass"
+      user.password_confirmation = "pass"
+      expect(user).not_to be_valid
+      expect(user.errors[:password].first).to include("must be at least 8 characters")
+    end
+
+    it "validates that the password meets the minimum strength requirements" do
+      user.password = "password"
+      user.password_confirmation = "password"
+      expect(user).not_to be_valid
+      expect(user.errors[:password].first).to include("at least one uppercase character, one lowercase character and one number")
+    end
+  end
+
+  describe "#email" do
+    it "validates the length of the email" do
+      user.email = "a" * 256
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("is too long (maximum is 255 characters)")
+    end
+  end
+end
