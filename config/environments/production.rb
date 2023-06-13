c29bfe8b48bb0d23f218e1d26a423168d81aad90
Leonid Brujev <brujev@gmail.com>
2023-06-13 10:44:18 +0100

    Reconfiguring Uglifier dependency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.serve_static_files = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Rather than use a CSS compressor, use the SASS style to perform compression
