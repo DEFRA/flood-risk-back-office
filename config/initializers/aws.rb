c2b0c695b54dfad6c38147011b099a1695a06c7a
brujeo <brujev@gmail.com>
2023-12-18 12:10:04 +0000

    adjusting aws config settings (#721)
    * adjusting aws config settings

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
+require "defra_ruby/aws"
+
 secrets = Rails.application.secrets
 
-Aws.config.update({
-  region: secrets.aws_region,
-  credentials: Aws::Credentials.new(secrets.aws_access_key_id, secrets.aws_secret_access_key)
-})
+DefraRuby::Aws.configure do |c|
+  epr_reports_bucket = {
+    name: FloodRiskBackOffice::Application.config.epr_reports_bucket_name,
+    region: secrets.aws_region,
+    credentials: {
+      access_key_id: secrets.aws_access_key_id,
+      secret_access_key: secrets.aws_secret_access_key
+    }
+  }
+
+  c.buckets = [epr_reports_bucket]
+end
