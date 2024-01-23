b1a430b6eb215297cca851541a96a105f3093618
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-01-23 18:52:49 +0000

    gem updates and lint fixes

--- spec/features/new_registration_spec.rb
+++ spec/features/new_registration_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe "New scenario" do
     expect(page).to have_css("h2", text: "Before you register you must")
 
     # check that the navigation links are no longer visible
-    expect(page).not_to have_css("#navigation")
+    expect(page).to have_no_css("#navigation")
   end
 end
