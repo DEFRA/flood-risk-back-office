34281dabd1f34b895f63dd66ff192634ef793e91
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-02-19 13:38:03 +0000

    Use defra_ruby_validators postcode validator

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -6,6 +6,5 @@ RSpec.describe Address do
     it { is_expected.to validate_presence_of(:street_address) }
     it { is_expected.to validate_presence_of(:city) }
     it { is_expected.to validate_presence_of(:postcode) }
-    it { is_expected.to validate_length_of(:postcode).is_at_most(8) }
   end
 end
