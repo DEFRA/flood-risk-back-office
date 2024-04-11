42844666e2b40946210b699570dd13339875c70f
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-04-11 20:46:48 +0100

    Remove area creation logic from missing_area rake task spec

--- spec/lib/tasks/lookups_spec.rb
+++ spec/lib/tasks/lookups_spec.rb
@@ -4,27 +4,22 @@ RSpec.describe "Lookups task" do
   describe "lookups:update:missing_area" do
     include_context "rake"
 
+    let(:location_to_update) { create(:location) }
+    let(:location_not_to_update) { create(:location, grid_reference: nil) }
     let(:run_for) { 10 }
 
     before do
-      expect(FloodRiskBackOffice::Application.config).to receive(:area_lookup_run_for).and_return(run_for)
+      location_to_update
+      location_not_to_update
+
+      allow(FloodRiskEngine::UpdateWaterManagementAreaJob).to receive(:perform_now).with(anything)
     end
 
-    it "update area info into locations missing it" do
-      location_to_update = create(:location)
-      area = double(:area, code: "123", area_id: 123, area_name: "Foo", short_name: "Bar", long_name: "Baz")
-      result = double(:result, areas: [area], successful?: true)
-      water_management_area_count = FloodRiskEngine::WaterManagementArea.count + 1
-
-      expect(DefraRuby::Area::WaterManagementAreaService).to receive(:run).and_return(result)
+    it "updates area info for a location without an area" do
+      expect(FloodRiskEngine::UpdateWaterManagementAreaJob).to receive(:perform_now).with(location_to_update)
+      expect(FloodRiskEngine::UpdateWaterManagementAreaJob).not_to receive(:perform_now).with(location_not_to_update)
 
       subject.invoke
-
-      expect(FloodRiskEngine::WaterManagementArea.count).to eq(water_management_area_count)
-
-      new_water_management_area = FloodRiskEngine::WaterManagementArea.last
-
-      expect(location_to_update.reload.water_management_area).to eq(new_water_management_area)
     end
   end
 end
