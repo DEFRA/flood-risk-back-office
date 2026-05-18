c513a4290d617790a87fbf7aa2f31701ae23f9bf
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2026-05-18 10:42:06 +0100

    Update bundle dependencies

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "Temporary welcome feature" do
   it "Visiting the home-page shows the User 'sign in' form" do
     visit root_path
-    expect(page).to have_content "Sign in"
+    expect(page).to have_text "Sign in"
   end
 end
