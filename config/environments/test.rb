9b27b3e8e9a755904671ea6b7a7be02ca9e03e73
Leonid Brujev <brujev@gmail.com>
2026-09-17 12:44:09 +0100

    Upgrade Rails configuration to version 8.1 and adjust environment settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,16 +5,12 @@ Rails.application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+  config.enable_reloading = false
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
-
-  # Configure static file server for tests with Cache-Control for performance.
-  config.serve_static_files   = true
-  config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
